e2a11a3b3184d85e254fd2a4c399eec0089bbdea
Abbas <abbas.naderi.1388@gmail.com>
2026-06-09 23:18:49 +0330

    Refactor BookDto to support multiple authors/translators
    Updated the BookDto class to replace the single `AuthorId` property with a `List<int> AuthorIds` to allow multiple authors. Added a new `List<int> TranslatorIds` property to support multiple translators. Removed unused properties `Title`, `Isbn`, and `CategoryId` to streamline the class. These changes enhance flexibility and better align with the application's requirements.

--- BookStoreApp.BLL/DTOs/BookDto.cs
+++ BookStoreApp.BLL/DTOs/BookDto.cs
@@ -9,17 +9,12 @@ namespace BookStoreApp.BLL.DTOs
     public class BookDto
     {
         public int? Id { get; set; }
-
         public string Title { get; set; }
-
         public string Isbn { get; set; }
-
-        public int AuthorId { get; set; }
-
-        public int PublisherId { get; set; }
-
         public int CategoryId { get; set; }
-
+        public int PublisherId { get; set; }
         public decimal Price { get; set; }
+        public List<int> AuthorIds { get; set; } =new List<int>();
+        public List<int> TranslatorIds { get; set; } = new List<int>();
     }
 }
